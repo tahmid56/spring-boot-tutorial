56cb805278477a5ec6f5abec5ba626178adc7a29
Tahmid Akter <tahmidakter56@gmail.com>
2024-01-07 23:12:49 +0600

    init

---
+++ src/main/java/com/example/contentcalender/ContentCalenderApplication.java
@@ -0,0 +1,13 @@
+package com.example.contentcalender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentCalenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentCalenderApplication.class, args);
+    }
+
+}
